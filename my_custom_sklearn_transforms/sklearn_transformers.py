00f12234ee2258f3a143a67872093870b536b2a6
Riperoso <65400107+Riperoso@users.noreply.github.com>
2020-08-21 20:23:33 -0300

    Add files via upload

---
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -0,0 +1,22 @@
+from sklearn.base import BaseEstimator, TransformerMixin
+
+
+# All sklearn Transforms must have the `transform` and `fit` methods
+class DropColumns(BaseEstimator, TransformerMixin):
+    def __init__(self, columns):
+        self.columns = columns
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        # Primeiro realizamos a cópia do dataframe 'X' de entrada
+        data = X.copy()
+        # Retornamos um novo dataframe sem as colunas indesejadas
+        return data.drop(labels=self.columns, axis='columns')
+
+    clf = DecisionTreeClassifier(criterion="entropy", max_depth=8)
+
+    clf = clf.fit(X_train,y_train)
+
+    y_pred = clf.predict(X_test)      
